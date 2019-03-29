700756c9c87395376ff51fd76f45d8d2a2054e53
Zoncharne Lee <zoncharne@gmail.com>
2019-03-29 21:26:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   
   def kind
     impossibru
-    if side1 == side2 == side3
+    if side1 == side2 && side2 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side3 ==  side1
       :isoceles

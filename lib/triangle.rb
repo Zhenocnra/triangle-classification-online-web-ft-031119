3dd219c793cce9f580591c7c7bf9fb2f14c89b71
Zoncharne Lee <zoncharne@gmail.com>
2019-03-29 21:26:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     impossibru
     if side1 == side2 && side2 == side3
       :equilateral
-    elsif side1 == side2 || side2 == side3 || side3 ==  side1
+    elsif side1 == side2 || side2 == side3 || side3 == side1
       :isosceles
     else
       :scalene

a61d324d418c5d16cf0acb4731ce2789bb390a7c
Zoncharne Lee <zoncharne@gmail.com>
2019-03-29 21:32:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,9 +19,7 @@ class Triangle
   end
   
   def impossibru
-    if (side1 <= 0 || side2 <= 0 || side3 <= 0)
-      raise TriangleError
-      elsif ((side1 + side2 < side3) || (side2 + side3 < side1) || (side1 + side3 < side1))
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0) || ((side1 + side2 < side3) || (side2 + side3 < side1) || (side1 + side3 < side1))
       raise TriangleError
     end
   end

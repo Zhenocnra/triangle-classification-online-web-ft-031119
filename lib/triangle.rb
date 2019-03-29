180e818b892d0fa8a2c4c3ddf2275f96d048dc98
Zoncharne Lee <zoncharne@gmail.com>
2019-03-29 21:23:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,9 @@ class Triangle
     if side1 == side2 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side3 ==  side1
-      :iso
-    
+      :isoceles
     else
+      :scalene
     end
   end
   
